116777e71c9f4b181413500ab51e930c1e08bd6f
MikOfHail <mikkisfreak@gmail.com>
2025-02-03 20:42:05 +0000

    set up page navigation

---
+++ astro.js
@@ -0,0 +1,8 @@
+function setCityStateVariables() {
+    const city = document.getElementById('cityInput').value;
+    const state = document.getElementById('stateInput').value;
+    console.log('User input:', city, state);
+    if (true) {
+        window.location.href = 'calendar.html';
+    }
+}
